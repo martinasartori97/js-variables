8e46b3dfc859786ee05e83f74757885c51fd4cff
martinasartori97 <martinasartori9@gmail.com>
2024-09-27 16:41:53 +0200

    edit js

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,26 +6,27 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = 8 + 16;
-console.log(sum); // Output: 24
+const numOnePlusNumTwo = numOne + numTwo;
+console.log(plus); // Output: 24
 
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
-const numThreeTimesNumTwo = 32 * 16;
-console.log(multify); // Output: 512
+const numThreeTimesNumTwo = numthree * numTwo;
+console.log(times); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = 32 / 8;
-console.log(); // Output: 4
+const numThreeDividedByNumOne = numthree / numone;
+console.log(divide); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = 32 - 8;
-console.log(numThreeMinusNumOne); // Output: 4
+const numThreeMinusNumOne = numthree - numone;
+console.log(minus); // Output: 4
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = 24 + 32;
+const sum = numone + numtwo + numthree;
 console.log(sum); // Output: 56
 
-// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = 56 / 8;
-console.log(); // Output: 7
+
+// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne.
+const numBytes = numone + numtwo + numthree / numone;
+console.log(numBytes); // Output: 7
