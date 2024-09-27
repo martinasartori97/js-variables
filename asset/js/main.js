afd088923f8f921d95a6ee28afab85aaa4cfb0c1
martinasartori97 <martinasartori9@gmail.com>
2024-09-27 15:27:43 +0200

    initial commit

---
+++ asset/js/main.js
@@ -0,0 +1,79 @@
+console.log("ciao");
+console.log("ciao");
+
+/* data types (primitives)
+-strings
+-numbers
+*/
+
+/* strings */
+"ciao 1"
+/* this is another string*/
+"ciao 2"
+
+console.log("ciao 2");
+console.log("ciao 2");
+
+console.log("4");
+
+
+/* numbers*/
+
+4
+5
+6
+
+console.log(4);
+console.log(5);
+console.log(6);
+
+// variable declaration
+let first_name;
+var last_name;
+
+// print a variable into the console
+console.log(First_name,last_name);
+
+// attach a value to the variable
+first_name = "Luca";
+console.log(first_name);
+
+//assegnazione
+First_name = "marco";
+console.log(First_name);
+
+last_name = "bianchi";
+
+
+// variable initialization
+const age =30;
+console.log(age);
+
+let my_cat_name = "antifa";
+
+console.log(my_cat_name);
+
+
+let first_name = "Martina"
+let sur_name = "Sartori"
+console.log(first_name, sur_name);
+
+let full_name = "my name is" + "  " + first_name + "  " + sur_name;
+
+const x = 5;
+let y = x+2;
+let sum = x+y ;
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
